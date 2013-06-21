bdc1a78babeb92328bd8cdbb290d2612f47affb0
Crossfire77 <ketan.solanki111@gmail.com>
2013-06-21 12:40:40 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,8 @@
 TARGET_KERNEL_CONFIG := cyanogenmod_p500_p509_defconfig
 BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p500 no_console_suspend
 
+TARGET_CPU_ABI := armeabi
+
 TARGET_BOOTLOADER_BOARD_NAME := p500
 TARGET_OTA_ASSERT_DEVICE := thunderg,p500
 
